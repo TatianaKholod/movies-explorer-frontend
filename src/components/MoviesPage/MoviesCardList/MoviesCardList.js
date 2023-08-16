01a1264d600737d734e1af56c920917e71d00314
Tatiana Kholod <tkholod@yandex.ru>
2023-08-16 10:09:36 +0300

    build пагинация для level-3

--- src/components/MoviesPage/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesPage/MoviesCardList/MoviesCardList.js
@@ -9,12 +9,6 @@ function MoviesCardList({ moviesCardArr, isLoading }) {
   // подсчет реально отображенных в контейнере карточек без скрытых
   const getCountDisplayedCards = (gridElem) => {
     if (!gridElem) return 0;
-    // так можно получить количество строк и столбцов грида
-    /*const styles = window.getComputedStyle(gridElem); // получаем все стили элемента
-    const width = styles.getPropertyValue('grid-template-rows');
-    console.log(width);*/
-    // так можно установить высоту 
-    //gridElem.parentElement.style.setProperty('max-height', '300px');
     const maxTop =
       gridElem.parentElement.offsetTop + gridElem.parentElement.offsetHeight;
     return Array.from(gridElem.children).reduce(
@@ -23,15 +17,57 @@ function MoviesCardList({ moviesCardArr, isLoading }) {
     );
   };
 
-  //  Для определения количества реально отображенных карточек
+  //  Для хранения реально отображенных карточек
   const [countDisplayedCards, setCountDisplayedCards] = useState(0);
-    
-   useEffect(() => {
-     setCountDisplayedCards(getCountDisplayedCards(refGrid.current));
+
+  useEffect(() => {
+    setCountDisplayedCards(getCountDisplayedCards(refGrid.current));
   }, [refGrid]);
+  
+  // Для определения кол-ва колонок и высоты элементов грида
+  const determinePropertiesGrid = () => {
+    // получаем все стили элемента
+    const stylesGrid = window.getComputedStyle(refGrid.current); 
+    const columns = stylesGrid.getPropertyValue('grid-template-columns');
+    const rows = stylesGrid.getPropertyValue('grid-template-rows');
+    const gap = stylesGrid.getPropertyValue('row-gap');
+    // уберем px из полученных значений свойств
+    const heightCard =
+      +rows.slice(0, rows.indexOf('px')) + +gap.slice(0, gap.indexOf('px'));
+    return { countColumns: (columns.match(/px/g) || []).length, heightCard };
+  };
 
+  // Для определения высоты контейнера
+  const determineHeightConteiner = (countColumns, heightCard, countCards) => {
+    // 70 это padding По хорошему нужно брать из свойств TODO
+    return (+Math.ceil(countCards / countColumns) * heightCard + +70)  + 'px';
+  };
+
+  const updateHeightContainer = () =>{
+    const { countColumns, heightCard } = determinePropertiesGrid();
+    setTimeout(refGrid.current.parentElement.style.setProperty(
+      'max-height',
+      determineHeightConteiner(countColumns, heightCard, countDisplayedCards)
+    ), 500);
+  }
+
+  // Для изменения размеров контейнера при resize
+  useEffect(() => {
+    window.addEventListener("resize", updateHeightContainer);
+    return () => window.removeEventListener("resize", updateHeightContainer);
+  });
+
+  // Обработчик нажатия кнопки Еще
   const handleOnClikMore = () => {
-
+     const { countColumns, heightCard } = determinePropertiesGrid();
+    const newCountCards =
+      +(countColumns > 1 ? countColumns : 2) + countDisplayedCards;
+    // приращение в гриде в одну колонку = 2
+    refGrid.current.parentElement.style.setProperty(
+      'max-height',
+      determineHeightConteiner(countColumns, heightCard, newCountCards)
+    );
+    setCountDisplayedCards(newCountCards);
   };
 
   return (
